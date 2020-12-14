e53e66e4546b80585fcafe458960ca9de704033a
Susilo Hartomo <susilo.hartomo@clozette.co>
2020-12-14 19:43:24 +0700

    update day_5

--- day_5/index.js
+++ day_5/index.js
@@ -1,54 +1,49 @@
 var items = [
-	['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpeg'],
-	['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpeg'],
+	['001', 'Keyboard Logitek', 60000, 'Keyboard yang mantap untuk kantoran', 'logitek.jpg'],
+	['002', 'Keyboard MSI', 300000, 'Keyboard gaming MSI mekanik', 'msi.jpg'],
 	['003', 'Mouse Genius', 50000, 'Mouse Genius biar lebih pinter', 'genius.jpeg'],
-	['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpeg'],
+	['004', 'Mouse Jerry', 30000, 'Mouse yang disukai kucing', 'jerry.jpg'],
 ];
 
-var img = ['image/logitek.jpg', 'image/msi.jpg', 'image/genius.jpeg', 'image/jerry.jpg'];
-
-function generateCard(item, img) {
+function generateCard(item) {
 	let hasil = `<div class="col-3">
                   <div class="card w-100 p-2" style="height: 360px;">
-                      <img src="${img}" class="card-img-top" alt="..." height="140px" >
+                      <img src="image/${item[4]}" class="card-img-top" alt="..." height="140px" >
                       <div class="card-body">
-                          <h5 class="card-title" id="itemName">${item[0]}</h5>
+                          <h5 class="card-title" id="itemName">${item[1]}</h5>
                           <p class="card-text" id="itemDesc">${item[3]}</p>
                           <p class="card-text">Rp ${item[2]}</p>
-                          <a href="#" class="btn btn-primary" id="addCart">Add to Cart</a>
+                          <a href="#" class="btn btn-primary" onclick={addChart()} id="addCart">Add to Cart</a>
                       </div>
                   </div>
                 </div>`;
 	return hasil;
 }
 
-let element = '';
-for (let i = 0; i < items.length; i++) {
-	element += generateCard(items[i], img[i]);
+function printInnerHtml(items) {
+	let element = '';
+	for (let i = 0; i < items.length; i++) {
+		element += generateCard(items[i]);
+	}
+
+	document.getElementById('listBarang').innerHTML = element = `<div class="row">${element}</div>`;
 }
 
-document.getElementById('listBarang').innerHTML = element = `<div class="row">${element}</div>`;
+printInnerHtml(items);
 
 var form = document.getElementById('formItem');
-form.addEventListener('submit', function (event) {
+form.addEventListener('keyup', function (event) {
 	event.preventDefault();
 	var search = document.getElementById('keyword').value;
 
 	let filter = [];
-	let index = [];
 	for (let i = 0; i < items.length; i++) {
 		if (items[i][1].toLowerCase().match(search.toLowerCase())) {
 			filter.push(items[i]);
-			index.push(i);
 		}
 	}
-	console.log('index: ', index);
 
-	let element = '';
-	for (let i = 0; i < filter.length; i++) {
-		element += generateCard(filter[i], img[index[i]]);
-	}
-	document.getElementById('listBarang').innerHTML = element = `<div class="row">${element}</div>`;
+	printInnerHtml(filter);
 });
 
 var cart = document.getElementById('cart');
