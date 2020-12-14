5d03208529d671e3cccf969dbf71fbef64e661d1
Susilo Hartomo <susilo.hartomo@clozette.co>
2020-12-14 18:17:41 +0700

    day 6 : php string and array

--- day_5/latihanJumatDes.js
+++ day_5/latihanJumatDes.js
@@ -1,17 +1,124 @@
-let alamat = 'gang haji boang 148, jalan kalimantan 1 nomor 71, kecamatan jombang, kelurahan ciputat, tangsel 15414';
+// let alamat = 'gang haji boang 148, jalan kalimantan 1 nomor 71, kecamatan jombang, kelurahan ciputat, tangsel 15414';
 
-function vokal(alamat) {
-	let jumlahVokal = 0;
+// function vokal(alamat) {
+// 	let jumlahVokal = 0;
 
-	for (let i = 0; i < alamat.length; i++) {
-		if (alamat[i] === 'a' || alamat[i] === 'e' || alamat[i] === 'i' || alamat[i] === 'u' || alamat[i] === 'o') {
-			jumlahVokal++;
-		}
+// 	for (let i = 0; i < alamat.length; i++) {
+// 		if (alamat[i] === 'a' || alamat[i] === 'e' || alamat[i] === 'i' || alamat[i] === 'u' || alamat[i] === 'o') {
+// 			jumlahVokal++;
+// 		}
+// 	}
+// 	return jumlahVokal;
+// }
+
+// let banyaknya = vokal(alamat);
+
+// // console.log(banyaknya);
+
+// // ==============
+
+// let alamat2 = 'gang haji boang 148, jalan kalimantan 1 nomor 71, kecamatan jombang, kelurahan ciputat, tangsel 15414';
+
+// function tambahAngka(alamat2) {
+// 	let hasil1 = 0;
+
+// 	for (let i = 0; i < alamat2.length; i++) {
+// 		if (alamat2[i] > 0) {
+// 			hasil1 = hasil1 + Number(alamat2[i]);
+// 			console.log(typeof alamat2[i]);
+// 		}
+// 	}
+//
+// 	return tambahAngka;
+// }
+// tambahAngka(alamat2);
+
+// =====================
+
+// let alamat3 = 'gang haji boang 148, jalan kalimantan 1 nomor 71, kecamatan jombang, kelurahan ciputat, tangsel 15414';
+
+// function tambahAngka(alamat3) {
+// 	let hasil2 = '';
+// 	let hasil3 = 0;
+
+// 	for (let i = 0; i < alamat3.length; i++) {
+// 		if (alamat3[i] > 0) {
+// 			hasil2 = hasil2 + alamat3[i];
+// 		} else {
+// 			hasil3 += Number(hasil2);
+
+// 			hasil2 = '';
+// 		}
+// 	}
+
+// 	if (hasil2) {
+// 		hasil3 += Number(hasil2);
+// 	}
+
+// 	// console.log(hasil2);
+// 	console.log(hasil3);
+
+// 	return tambahAngka;
+// }
+// tambahAngka(alamat3);
+
+// ==============
+
+let dataSiswa = [
+	['budi', 'ani', 'iwan', 'susi', 'andi'],
+	['1a', '2d', '3a', '2d', '1b'],
+	[80, 68, 90, 100, 77],
+];
+
+// let dataSiswa2 = [
+// 	['budi', 'ani', 'iwan', 'susi', 'andi', 'banu'],
+// 	['1a', '2d', '3a', '2d', '1b', '3a'],
+// 	[100, 68, 90, 100, 77, 76],
+// ];
+
+// // Buat nilai rata-rata siswa
+// function nilaiRata2(arr) {
+// 	let totalNilai = 0;
+
+// 	for (let i = 0; i < arr[2].length; i++) {
+// 		totalNilai += arr[2][i];
+// 	}
+
+// 	let nilaiRataRata = 0;
+// 	nilaiRataRata = totalNilai / arr[2].length;
+
+// 	return nilaiRataRata.toFixed(2);
+// }
+
+// let nilaiRataRata = nilaiRata2(dataSiswa2);
+
+// console.log(nilaiRataRata);
+
+let dataSiswa2 = [
+	['budi', 'ani', 'iwan', 'susi', 'andi', 'banu'],
+	['1a', '2d', '3a', '2d', '1b', '3a'],
+	[100, 68, 90, 100, 77, 76],
+];
+
+function mengelompokkanSiswa(bahan) {
+	let hasil = [];
+
+	for (let i = 0; i < bahan[0].length; i++) {
+		let siswa = {
+			nama: '',
+			kelas: '',
+			nilai: 0,
+		};
+
+		siswa.nama = bahan[0][i];
+		siswa.kelas = bahan[1][i];
+		siswa.nilai = bahan[2][i];
+		hasil.push(siswa);
 	}
-
-	return jumlahVokal;
+	return hasil;
 }
 
-let banyaknya = vokal(alamat);
-console.log(banyaknya);
-//'gang haji boang 148, jalan kalimantan 1 nomor 71, kecamatan jombang, kelurahan ciputat, tangsel 15414';
+let hasilAkhir = mengelompokkanSiswa(dataSiswa2);
+console.log(hasilAkhir);
+
+console.log(mengelompokkanSiswa(dataSiswa));
---
+++ day_6/array.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Array</title>
+</head>
+
+<body>
+  <h1>Berlatih Array</h1>
+
+  <?php
+  echo "<h3> Soal 1 </h3>";
+  /* 
+      SOAL NO 1
+      Kelompokkan nama-nama di bawah ini ke dalam Array.
+      Kids : "Mike", "Dustin", "Will", "Lucas", "Max", "Eleven" 
+      Adults: "Hopper", "Nancy",  "Joyce", "Jonathan", "Murray"
+  */
+  $kids = array("Mike", "Dustin", "Will", "Lucas", "Max", "Eleven"); // Lengkapi di sini
+  $adults = array("Hopper", "Nancy",  "Joyce", "Jonathan", "Murray");
+  echo "<label>Kids in Array : </label><br>";
+  printf(implode(", ", $kids));
+  echo "<br><br><label>Adults in Array :</label><br>";
+  printf(implode(", ", $adults));
+  echo "<h3> Soal 2</h3>";
+  /* 
+      SOAL NO 2
+      Tunjukkan panjang Array di Soal No 1 dan tampilkan isi dari masing-masing Array.
+  */
+  echo "Cast Stranger Things: ";
+  echo "<br>";
+  echo "Total Kids: " . count($kids) . ""; // Berapa panjang array kids
+  echo "<br>";
+  echo "<ol>";
+  echo "<li> $kids[0] </li>";
+  echo "<li> $kids[1] </li>";
+  echo "<li> $kids[2] </li>";
+  echo "<li> $kids[3] </li>";
+  echo "<li> $kids[4] </li>";
+  echo "<li> $kids[5] </li>";
+  // Lanjutkan
+
+  echo "</ol>";
+
+  echo "Total Adults: " . count($adults) . ""; // Berapa panjang array adults
+  echo "<br>";
+  echo "<ol>";
+  // Lanjutkan
+  for ($x = 0; $x < count($adults); $x++) {
+    echo "<li> $adults[$x] </li>";
+  }
+  echo "</ol>";
+
+  /*
+            SOAL No 3
+            Susun data-data berikut ke dalam bentuk Asosiatif Array (Array Multidimensi)
+            
+            Name: "Will Byers"
+            Age: 12,
+            Aliases: "Will the Wise"
+            Status: "Alive"
+
+            Name: "Mike Wheeler"
+            Age: 12,
+            Aliases: "Dungeon Master"
+            Status: "Alive"
+
+            Name: "Jim Hopper"
+            Age: 43,
+            Aliases: "Chief Hopper"
+            Status: "Deceased"
+
+            Name: "Eleven"
+            Age: 12,
+            Aliases: "El"
+            Status: "Alive"
+            
+        */
+
+  $users = array(
+    array("Name" => "Will Byers", "Age" => 12, "Aliases" => "Will the Wise", "Status" => "Alive"),
+    array("Name" => "Mike Wheeler", "Age" => 12, "Aliases" => "Dungeon Master", "Status" => "Alive"),
+    array("Name" => "Jim Hopper", "Age" => 43, "Aliases" => "Chief Hopper", "Status" => "Deceased"),
+    array("Name" => "Eleven", "Age" => 12, "Aliases" => "El", "Status" => "Alive")
+  );
+
+  echo "<p>Users</p>";
+  echo "<ol>";
+  for ($i = 0; $i < count($users); $i++) {
+    # code...
+    echo "<li><ul>",
+      "<li> Name : " . $users[$i]["Name"] . "</li> ",
+      "<li> Age : " . $users[$i]["Age"] . "</li> ",
+      "<li> Aliases : " . $users[$i]["Aliases"] . "</li> ",
+      "<li> Status : " . $users[$i]["Status"] . "</li> ",
+      "</ul></li><br>";
+  }
+  echo "</ol>";
+  ?>
+</body>
+
+</html>
